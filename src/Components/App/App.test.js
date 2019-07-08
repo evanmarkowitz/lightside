ad12aff5ff48c3215250805f1203cf6455290864
RyanFlachman <ryan@flachman.net>
2019-07-08 09:10:38 -0600

    Add app testing

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,16 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme'
 import App from './App';
+import newObjs from './App.helper'
+import peopleData from '../../Data/PeopleTestData'
 
 describe('App', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(<App />)
   })
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+  it('should have be able to store people data in state', () => {
+    expect(wrapper.state().people).toEqual([])
+  })
+
+  it('should be able to store planet data', () => {
+    expect(wrapper.state().planets).toEqual([])
+  })
+
+  it('should be able to store vehicle data', () => {
+    expect(wrapper.state().planets).toEqual([])
+  })
+})
+
+describe('App Helper', () => {
+  let newObjsFunc, attributes, wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+    attributes = ['name', 'birth_year', 'gender', 'height', 'eye_color'];
+    newObjsFunc = newObjs(peopleData, attributes, 'people', wrapper)
+  })
 })
   
--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import Card from './Card'
 
-describe('Card', () => {
-  
+describe.skip('Card', () => {
+  it('should should have a test', () => {
+    
+  })
 })
